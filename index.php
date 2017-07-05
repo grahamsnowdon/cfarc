2015b90646cd929dbad24a5ee9e28dc374c2997c
Graham Snowdon <graham@snowdon.me>
2017-07-05 21:12:01 +0100

    Index and login 80 chars
    Reduced index and login line length in accordance with best practise to
    80 chars or less.
    Still no fix to displaying index. Will check with test script and work
    from there.

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
-include 'login.php';
-//This script is the landing page, the customer has recently requested client specific landing pages, this will act as a base for that and each 
-// customer will have a branded landing page, this page acts as a template for that.
+include ($_SERVER['DOCUMENT_ROOT'].'login.php');
+//echo "Hi developer!";
+//This script is the landing page, the customer has recently requested client
+//specific landing pages, this will act as a base for that and each
+// customer will have a branded landing page, this page acts as a template
+//for that.
  ?>
